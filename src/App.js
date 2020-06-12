38e011fe660afb2ee5a5a1fbd23e5430fe88ed66
Marina <marinafaenze@hotmail.com>
2020-06-12 11:16:23 -0300

    body style added to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Login from './pages/Login/Login'
 
 function App() {
   return (
     <div className="App">
+      <Login />
     </div>
   );
 }
