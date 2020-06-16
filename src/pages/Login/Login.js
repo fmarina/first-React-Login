e87aaf44c3db4e25a48c3b7bf2d7e7c763ce580b
Marina <marinafaenze@hotmail.com>
2020-06-16 09:04:57 -0300

    localStorage and hasError

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -5,10 +5,12 @@ import Label from './components/Label/Label';
 import Input from './components/Input/Input'
 
 const Login = () => {
-    // Constante array donde user almacena mi state y setUser funcion encargada de actualizar ese state.
+    // Constante array donde se almacena mi state y setState quien es el encargado de actualizar ese state.
     const [user, setUser] = useState('');
     const [password, setPassword] = useState("");
     const [passwordError, setPasswordError] = useState(false);
+    const [isLogin, setIsLogin] = useState(false);
+    const [hasError, setHasError] = useState(false);
 
     function handleChange(name, value) {
         (name === "usuario") 
@@ -17,14 +19,36 @@ const Login = () => {
         : setPasswordError(false); setPassword(value); 
     }
 
+    function ifMatch(param) {
+        const {user, password} = param;
+        if(user.length > 0 && password.length > 0) {
+            if(user === "admin" && password === "123456") {
+                let ac = { user, password};
+                console.log("ac.user", user);
+                let account = JSON.stringify(ac);
+                localStorage.setItem("account", account);
+                setIsLogin(true); 
+            } 
+            else {
+                setIsLogin(false);
+                setHasError(true);
+            }
+        } 
+        else {
+            setIsLogin(false);
+            setHasError(true);
+        }        
+    }
+
     function handleSubmit() {
         let account = { user, password};
-        (account)&& console.log(account);
+        (account) && ifMatch(account);
     }
 
     return (
         <div className="login-container">
-            <div className="login-content">
+            {!isLogin && 
+                <div className="login-content">
                 <Title text="¡Bienvenido!"/>
                 <Label text="Usuario"/>
                 <Input 
@@ -51,13 +75,20 @@ const Login = () => {
                 {passwordError &&                
                     <label className="label-error">Contraseña invalida o incorrecta</label>
                 }
+
+                {hasError &&
+                    <label className="label-haserror">El nombre de usuario o la contraseña son incorrectos, o no existe en nuestra base de datos</label>
+                }
+                
                 <div className="submit-button-container">
                     <button onClick={handleSubmit} className="submit-button">
                         Ingresar
                     </button> 
                 </div>
 
-            </div>                              
+            </div>
+            }
+                                          
         </div>
     );
 }
